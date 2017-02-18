16feda436da6050267b2e73648a6ed6975e3bac3
wangweiquan <Administrator@PC-20160606TZLO>
2017-02-18 20:17:51 +0800

    更新文件写入类 添加从文件获取数据的类

--- src/main/java/com/wwq/java/blog/file/PrintWriterDemo.java
+++ src/main/java/com/wwq/java/blog/file/PrintWriterDemo.java
@@ -1,7 +1,37 @@
 package com.wwq.java.blog.file;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 /**
  * Created by Administrator on 2017-2-17.
  */
+
+/**
+ * 使用PrintWriter类向文件中写入数据
+ */
 public class PrintWriterDemo {
+    public static void main(String[] args) {
+        try {
+
+            File file = new File("E:\\one.txt");
+            if (file.exists()) {
+                System.out.println("file already exists");
+                System.exit(0);
+            }
+
+            //文件不存在,可以放心地创建文件
+            PrintWriter output = new PrintWriter(file);
+            output.print("中国");
+            output.print("hello world");
+            output.println("hell line");
+            output.printf("你好吗 %d", 100);
+
+            output.flush();
+            output.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/main/java/com/wwq/java/blog/file/ScannerDemo.java
@@ -0,0 +1,36 @@
+package com.wwq.java.blog.file;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * Created by Administrator on 2017-2-18.
+ */
+
+/**
+ * 使用Scanner从文件中读取内容
+ *
+ * scanner.next(),nextByte(),nextShort(),nextInt(),nextLong(),nextFloat(),nextDouble()
+ * 等都是根据分隔符进行读取数据的,默认的分隔符是空格,可以使用方法useDelimiter(String regex)设置新的分隔符;
+ *
+ * 读取方式:首先跳过任意分隔符(默认空格),然后读取一个以分隔符结束的数据
+ */
+public class ScannerDemo {
+    public static void main(String[] args) {
+        File file = new File("E:\\one.txt");
+
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNext()) {
+                String first = scanner.next();
+                System.out.println(first);
+
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
